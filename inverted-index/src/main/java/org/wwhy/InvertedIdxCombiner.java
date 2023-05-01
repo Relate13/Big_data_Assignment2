2a2aaa853801890fa19f43eeda5b4a03fb3c33aa
Qingyun Yang <qingyun_yang@foxmail.com>
2023-05-01 13:53:13 +0800

    FIX: add some comments

--- inverted-index/src/main/java/org/wwhy/InvertedIdxCombiner.java
+++ inverted-index/src/main/java/org/wwhy/InvertedIdxCombiner.java
@@ -5,14 +5,20 @@ import org.apache.hadoop.mapreduce.Reducer;
 
 import java.io.IOException;
 
+// 对本地节点Map的结果进行预聚合
+// 输入 <词语/文档名, [词频]>
+// 输出 <词语, 词频/文档名>
 public class InvertedIdxCombiner extends Reducer<Text, Text, Text, Text> {
     @Override
     protected void reduce(Text key, Iterable<Text> values, Reducer<Text, Text, Text, Text>.Context context) throws IOException, InterruptedException {
+        // 统计单词在某一文档中出现的次数
         int sum = 0;
         for (Text v : values) {
             sum += Integer.parseInt(v.toString());
         }
+        // 从key中取得词语和文档名
         String[] wordWithFile = key.toString().split("/");
+        // 将词语作为新的key，将词频和文档名合并作为新的value
         context.write(new Text(wordWithFile[0]), new Text(sum +"/"+wordWithFile[1]));
     }
 }
--- inverted-index/src/main/java/org/wwhy/InvertedIdxReducer.java
+++ inverted-index/src/main/java/org/wwhy/InvertedIdxReducer.java
@@ -9,26 +9,41 @@ import java.util.HashSet;
 import java.util.Iterator;
 import java.util.Set;
 
+// 输入 <词语, [词频/文档名]>
+// 输出 <词语, 平均出现次数+[文档名: 词频]>
 public class InvertedIdxReducer extends Reducer<Text, Text, Text, Text> {
-
+    // 用于格式化词语平均出现次数，保留两位小数
     private static final DecimalFormat formatter = new DecimalFormat("0.00");
     @Override
     protected void reduce(Text key, Iterable<Text> values, Reducer<Text, Text, Text, Text>.Context context) throws IOException, InterruptedException {
+        // 用于统计有key出现的文件数量
         Set<String> fileSet = new HashSet<>();
+        // 用于统计key出现的总数
         int wordSum = 0;
+        // 倒排索引，格式为<文件名: 词频; ...>
         StringBuilder invertedIdx = new StringBuilder();
+        // 遍历所有value
         Iterator<Text> iter = values.iterator();
         while (iter.hasNext()) {
+            // 从value中取得在Combine阶段写入的文件名和词频
             String[] numWithFile = iter.next().toString().split("/");
+            // 将文件名加入set，利用set的不重复特性统计文件数量
             fileSet.add(numWithFile[1]);
+            // 统计词语出现总数
             wordSum += Integer.parseInt(numWithFile[0]);
+            // 写入倒排索引
             invertedIdx.append(numWithFile[1]).append(": ").append(numWithFile[0]);
+            // 如果是最后一个索引，则不写分号（与实验文档保持一致）
             if (iter.hasNext()) {
                 invertedIdx.append("; ");
             }
         }
+        // 计算平均出现次数
         double avgFrequency = (double)wordSum / fileSet.size();
+        // 保留两位小数，并将其拼接到倒排索引之前作为value
         String outValue = formatter.format(avgFrequency)+", "+ invertedIdx;
+        // 输出条目，格式为<[词语], 平均出现次数, 文档名: 词频; ...>
         context.write(new Text("["+key.toString()+"]"), new Text(outValue));
     }
 }
+
